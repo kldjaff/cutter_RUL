55041be80bd4e51520579ec48f441d71bd3d2868
Yi Hu <yi.hu@unitediot.com.cn>
2021-04-16 15:59:54 +0800

    adding files

--- start.py
+++ start.py
@@ -1,1 +1,54 @@
 # coding=utf-8
+import keras
+import keras.backend as K
+import pandas as pd
+import numpy as np
+import os
+import pandas as pd
+
+from sklearn import preprocessing
+
+# Setting seed for reproducibility
+np.random.seed(1234)
+PYTHONHASHSEED = 0
+
+# define path to save model
+model_path = '4regression_model.h5'
+
+# read training data - It is the aircraft engine run-to-failure data.
+train_df = pd.read_csv('data/train_FD001.txt', sep=" ", header=None)
+print(train_df)
+train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
+train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                    's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                    's15', 's16', 's17', 's18', 's19', 's20', 's21']
+train_df = train_df.sort_values(['id','cycle'])
+
+# read test data - It is the aircraft engine operating data without failure events recorded.
+test_df = pd.read_csv('data/test_FD001.txt', sep=" ", header=None)
+test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
+test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                   's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                   's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+# read ground truth data - It contains the information of true remaining cycles for each engine in the testing data.
+truth_df = pd.read_csv('data/RUL_FD001.txt', sep=" ", header=None)
+truth_df.drop(truth_df.columns[[1]], axis=1, inplace=True)
+
+# Data Labeling - generate column RUL
+rul = pd.DataFrame(train_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+train_df = train_df.merge(rul, on=['id'], how='left')
+train_df['RUL'] = train_df['max'] - train_df['cycle']
+train_df.drop('max', axis=1, inplace=True)
+
+# MinMax normalization
+train_df['cycle_norm'] = train_df['cycle']
+cols_normalize = train_df.columns.difference(['id','cycle','RUL'])
+min_max_scaler = preprocessing.MinMaxScaler()
+norm_train_df = pd.DataFrame(min_max_scaler.fit_transform(train_df[cols_normalize]),
+                             columns=cols_normalize,
+                             index=train_df.index)
+
+join_df = train_df[train_df.columns.difference(cols_normalize)].join(norm_train_df)
+train_df = join_df.reindex(columns = train_df.columns)
